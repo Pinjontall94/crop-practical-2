6ded7af3064755b5c17232d277d9f2e3f37d20de
quesadillacat <aaroering@outlook.de>
2025-12-06 12:54:45 +0100

    R Scripts for EBLUPs

---
+++ R scripts for (E)BLUPS/CCM.R
@@ -0,0 +1,48 @@
+
+# Packages
+library(tidyr)
+library(dplyr)
+library(lme4)
+library(lmerTest)
+
+# Loading Data
+df <- read.csv2("CCM.csv", stringsAsFactors = TRUE)
+
+#Wide to Long
+df_long <- df %>%
+  pivot_longer(
+    cols = c(CCM_1, CCM_2, CCM_3, CCM_4, CCM_5, CCM_6),  # column names
+    names_to = "Rep",               # new column for rep
+    values_to = "CCM"               # new column for trait value
+  )
+
+df_long$Rep <- as.factor(df_long$Rep)
+
+head(df_long)
+
+# Looks good
+
+# Does Genotype influence CCM?
+
+model_fixed <- lmer(CCM ~ Genotype + (1|Rep), data = df_long)
+
+anova(model_fixed)
+
+# yaaay it does
+
+# BLUP and EBLUP calculations
+
+model_blup <- lmer(CCM ~ 1 + (1|Genotype) + (1|Rep), data = df_long)
+
+bl <- ranef(model_blup)$Genotype
+bl <- data.frame(
+  Genotype = rownames(bl),
+  BLUP = bl[,1]
+)
+
+grand_mean <- fixef(model_blup)[1]
+bl$EBLUP <- bl$BLUP + grand_mean
+
+print(bl)
+
+
